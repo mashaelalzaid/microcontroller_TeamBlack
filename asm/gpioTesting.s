eba8736e12efd2c79393a21fe6acd2988c67b580
BinAhmed-Elead <abdullahelead@gmail.com>
2025-02-24 09:27:34 +0300

    the new program qamar asked me to do

--- asm/gpioTesting.s
+++ asm/gpioTesting.s
@@ -1,14 +1,33 @@
-back:
 nop
 lui x11, 0x20000    # GPIO base address
 addi x11, x11, 0x100#
 lui x10, 0xF        # Load 0x0000FFFF for direction
 addi x10, x10, -1   #
 sw x10, 8(x11)      # Write to direction register (0x108)
-addi x10, x0, 0xF   # Set value 0xF
+######################################################################
+back:
+li x10, 0xF   # Set value 0xF
 sw x10, 4(x11)      # Write to output register (0x104)
-li x12, 0x10000000
-jalr x12             # Loop
+call delay
+li x10, 0xF0   # Set value 0xF0
+sw x10, 4(x11)      # Write to output register (0x104)
+call delay
+li x10, 0xF00  # Set value 0xF00
+sw x10, 4(x11)      # Write to output register (0x104)
+call delay
+li x10, 0xF000   # Set value 0xF000
+sw x10, 4(x11)      # Write to output register (0x104)
+call delay
+j back
 nop
 nop
 nop
+
+
+delay:
+    li t6, 2500000
+    delay_loop:
+        addi t6, t6, -1
+        bnez t6, delay_loop
+    ret
+   
